183eb5bf7e8bb69e252c9a011e91b096e9eb9a43
Muhammad Sidik <pasker.shinigami@gmail.com>
2020-09-17 21:42:59 +0700

    update post

--- api/controllers/PostController.go
+++ api/controllers/PostController.go
@@ -147,3 +147,77 @@ func (server *Server) DeletePost(w http.ResponseWriter, r *http.Request) {
 	responses.JSON(w, http.StatusNoContent, "")
 
 }
+
+func (server *Server) UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	pid, err := strconv.ParseUint(vars["id"], 10, 64)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	uid, err := auth.ExtractTokenId(r)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	post := models.Post{}
+	err = server.DB.Debug().Model(models.Post{}).Where("id = ?", pid).Take(&post).Error
+
+	if err != nil {
+		responses.ERROR(w, http.StatusNotFound, errors.New("Post not Found"))
+		return
+	}
+
+	if uid != post.AuthorID {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	postUpdate := models.Post{}
+
+	err = json.Unmarshal(body, &postUpdate)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	if uid != postUpdate.AuthorID {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	postUpdate.Prepare()
+
+	err = postUpdate.Validated()
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	postUpdate.ID = post.ID
+
+	postUpdated, err := postUpdate.UpdatePost(server.DB)
+
+	if err != nil {
+		formatError := formaterror.FormatError(err.Error())
+
+		responses.ERROR(w, http.StatusInternalServerError, formatError)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, postUpdated)
+}
